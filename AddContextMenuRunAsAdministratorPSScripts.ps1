feb16e1bc2264c77228146fcf3ea8d08ad320e5a
masterlamps <benjamin.haase@hotmail.com>
2020-02-25 19:23:04 +0100

    Add inilite script - add registry key entry for context menu to start ps1 script as admin

---
+++ AddContextMenuRunAsAdministratorPSScripts.ps1
@@ -0,0 +1,13 @@
+﻿
+$DisplayTextInContextMenu = "Run as Administrator"
+
+$hkcrPath = "HKCR:\Microsoft.PowerShellScript.1\Shell\$DisplayTextInContextMenu\Command"
+
+if((Test-Path $hkcrPath) -eq $false) {
+    #Create
+    Write-Host "Key Doesn't Exist - It will be created."
+    New-Item -Path $hkcrPath -Force | Out-Null
+
+    $psCommand = '"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "-Command" ""& {Start-Process PowerShell.exe -ArgumentList ''-ExecutionPolicy RemoteSigned -File \"%1\"'' -Verb RunAs}"'
+    New-ItemProperty -Path $hkcrPath -Name "(default)" -Value $psCommand -Force | Out-Null
+}
